50b5edbcb8203a171c5ca21380fbeb4d87ebcd48
Bhupesh1114 <78557475+Bhupesh1114@users.noreply.github.com>
2022-05-07 10:36:15 +0530

    react-app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import './App.css';
+import Cart from './Components/Cart/Cart';
+import Checkout from './Components/Checkout/Checkout';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="cart" element={<Cart />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Routes>
+      {/* <Cart /> */}
+      <Checkout />
+    </>
+  );
+}
+
+export default App;
